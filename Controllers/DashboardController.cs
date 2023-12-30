39fb70107479b87f4d115b3b6898646f6aacec00
travezvinueza <travezvinueza@gmail.com>
2023-12-30 18:07:18 -0500

    ultimos cambios

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -1,10 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
+using Agenda.ViewModels;
 using Agenda.Models.Domain;
-using Agenda.Models.Dto;
-using Agenda.Service.Abstract;
 
 namespace Agenda.Controllers
 {
@@ -12,72 +10,24 @@ namespace Agenda.Controllers
     public class DashboardController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
-        private readonly IUserAuthenticationService _userService;
 
-        public DashboardController(UserManager<ApplicationUser> userManager, IUserAuthenticationService userService)
+        public DashboardController(UserManager<ApplicationUser> userManager)
         {
             _userManager = userManager;
-            _userService = userService;
         }
 
         public IActionResult Display()
         {
-            return View();
-        }
-        //este metodo es para traer el usuario y y poder actualizarlo 
-        //con el siguiente metodo
-        [HttpGet]
-        public async Task<IActionResult> Update(string id)
-        {
-            // Obtener el usuario por su Id
-            var userModel = await _userService.GetById(id);
+            var username = User.Identity.Name;
+            var user = _userManager.FindByNameAsync(username).Result;
 
-            if (userModel == null)
+            var viewModel = new DashboardViewModel
             {
-                // Manejar el caso en el que el usuario no se pueda recuperar
-                return RedirectToAction("Display");
-            }
-            return View(userModel);
+                UserName = user.UserName,
+                ProfilePicture = user.ProfilePicture
+            };
+
+            return View(viewModel);
         }
-        //este es el suiguiente metodo que es para actualizarlo el usuario logueado
-        [HttpPost]
-        public async Task<IActionResult> UpdateAsync(string userId, RegistrationModel model)
-        {
-            var status = new Status();
-
-            // Obtener el usuario por su Id
-            var user = await _userManager.FindByIdAsync(userId);
-
-            if (user == null)
-            {
-                status.StatusCode = 0;
-                status.Message = "Usuario no encontrado";
-                // Debes crear una vista para mostrar errores
-                return View("ErrorView", status);
-            }
-
-            // Actualizar propiedades del usuario
-            user.FirstName = model.FirstName;
-            user.LastName = model.LastName;
-            user.Email = model.Email;
-            user.UserName = model.Username;
-            user.ProfilePicture = model.ProfilePicture;
-
-            // Actualizar el usuario en la base de datos
-            var result = await _userManager.UpdateAsync(user);
-
-            if (!result.Succeeded)
-            {
-                status.StatusCode = 0;
-                status.Message = "Error al actualizar el usuario";
-                // Debes crear una vista para mostrar errores
-                return View("ErrorView", status);
-            }
-
-            status.StatusCode = 1;
-            status.Message = "Usuario actualizado exitosamente";
-            return RedirectToAction("Display");
-        }
-
     }
 }
--- Service/Abstract/IUserAuthenticationService.cs
+++ Service/Abstract/IUserAuthenticationService.cs
@@ -1,5 +1,4 @@
-﻿
-using Agenda.Models.Domain;
+﻿using Agenda.Models.Domain;
 using Agenda.Models.Dto;
 
 
@@ -7,20 +6,14 @@ namespace Agenda.Service.Abstract
 {
     public interface IUserAuthenticationService
     {
-   
         Task<IList<string>> GetRolesAsync(string userId);
         Task<Status> LoginAsync(LoginModel model);
         Task LogoutAsync();
         Task<Status> RegisterAsync(RegistrationModel model);
         Task<Status> ChangePasswordAsync(ChangePassword model, string username);
 
-        //esto es para que me traiga una lista de los usuario
+        //traer una lista de usuarios
         List<RegistrationModel> GetAll();
 
-        //todo esto es necesario para actualizar el usuario metodo crud
-        Task<Status> UpdateAsync(string userId, RegistrationModel model);
-        Task<RegistrationModel> GetById(string id);
-        Task Add(RegistrationModel model);
-        Task Delete(string id);
     }
 }
